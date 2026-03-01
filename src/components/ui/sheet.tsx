a438e6c82e5ef802264c31ce31f065b9d4ca49f1
Dion Santoso <dionsantoso15@gmail.com>
2026-03-01 16:43:53 +0000

    change it into 500px

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -64,7 +64,7 @@ const SheetContent = React.forwardRef<
       className={cn(sheetVariants({ side }), className)}
       {...props}
     >
-      <SheetTitle className="sr-only">Menu Sidebar</SheetTitle>
+      <SheetPrimitive.Title className="sr-only">Menu Sidebar</SheetPrimitive.Title>
       {children}
       <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
         <X className="h-4 w-4" />
@@ -73,7 +73,7 @@ const SheetContent = React.forwardRef<
     </SheetPrimitive.Content>
   </SheetPortal>
 ))
-SheetContent.displayName = SheetContent.displayName
+SheetContent.displayName = "SheetContent"
 
 const SheetHeader = ({
   className,
