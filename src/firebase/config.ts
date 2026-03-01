24d5befc4c1e196d71bc07455813945bc81561d1
Dion Santoso <dionsantoso15@gmail.com>
2026-03-01 12:46:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5949057200-63df6",
+  "appId": "1:175370990926:web:0f291ad3fd4691142f1a1f",
+  "apiKey": "AIzaSyDMJ3bmp_SQNR1ONsBf3zGvhLmdEvRqgTw",
+  "authDomain": "studio-5949057200-63df6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "175370990926"
+};
